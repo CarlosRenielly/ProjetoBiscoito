cbc2d3037e01b4b0ee2d52665c37e2927dcd5c4a
Carlos-Renielly <many1458@gmail.com>
2023-04-15 12:06:37 -0300

    props
    entendedo as props

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,12 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {View, Text, Image} from 'react-native'
 
- export default function App(){
+function App(){
+  
+  let nome = 'Carlos Renielly'
+  
+  
+  
   return(
     <View>
-      <Text>
-        Olá mundo!
+      <Text>Olá Seja bem vindo!</Text>
+      <Text style={{color:'#FF0000', fontSize:25, margin: 15}}>Meu primeiro App</Text>
+      <Text style= {{fontSize: 18, color: 'green'}}>
+        Sujeito Programador
       </Text>
+
+
+      <Text style= {{fontSize:20}}>
+        {nome}
+      </Text>
+
+      <Logo Largura={300} altura={300} Fulano={'CARLÃO'}/>
+
     </View>
   )
 }
+
+  export default App
+
+
+
+  function Logo(props){
+    
+    let img = 'https://sujeitoprogramador.com/reactlogo.png'
+    
+    return(
+      <View>
+        <Image source={{uri: img}} style={{width: props.Largura, height: props.altura}}/>
+        <Text>{props.Fulano}</Text>
+      </View>
+    )
+  }
