25d4e26a6c4bed72ed0b3442e6a24b58c427dd23
Carlos-Renielly <many1458@gmail.com>
2023-04-16 11:17:44 -0300

    states

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,26 +1,20 @@
-import React from "react";
-import {View, Text, Image} from 'react-native'
+import React, {useState} from "react";
+import {View, Text, Button, Alert} from 'react-native'
 
 function App(){
-  
-  let nome = 'Carlos Renielly'
-  
-  
-  
+  const [nome, setNome] = useState('Carlos Ranielly');
+  const [idade, setIdade] = useState (29)
+
+  function entrar(nome, idade){
+    setNome(nome);
+    setIdade(idade);
+  }
+
   return(
-    <View>
-      <Text>Olá Seja bem vindo!</Text>
-      <Text style={{color:'#FF0000', fontSize:25, margin: 15}}>Meu primeiro App</Text>
-      <Text style= {{fontSize: 18, color: 'green'}}>
-        Sujeito Programador
-      </Text>
-
-
-      <Text style= {{fontSize:20}}>
-        {nome}
-      </Text>
-
-      <Logo Largura={300} altura={300} Fulano={'CARLÃO'}/>
+    <View style={{marginTop:25}}>
+      <Text style={{fontSize:20}}>{nome}</Text>
+      <Text style={{fontSize:20}}>{idade}</Text>
+      <Button title="Acessar" onPress={() => entrar('Homem esta la', 33)}/>
 
     </View>
   )
@@ -30,14 +24,4 @@ function App(){
 
 
 
-  function Logo(props){
-    
-    let img = 'https://sujeitoprogramador.com/reactlogo.png'
-    
-    return(
-      <View>
-        <Image source={{uri: img}} style={{width: props.Largura, height: props.altura}}/>
-        <Text>{props.Fulano}</Text>
-      </View>
-    )
-  }
+  
